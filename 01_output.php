4b5951d03dd1625b5d336ca251b32d249d2d8aa6
Habacuccc <cooper4evah@hotmail.com>
2022-09-25 23:15:07 -0500

    Rename index.php to 01_output.php and added 02_variables.php

---
+++ 01_output.php
@@ -0,0 +1,24 @@
+<?php
+
+    echo "Test1";
+
+    print_r([1,2,3]);
+
+    echo "\n\n";
+
+    var_dump(true);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo 'Post One'; ?> </h1>
+</body>
+</html>
